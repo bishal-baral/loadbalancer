fa760e168775d28028f4cd719abb747898f31577
bishal-baral <bishalbaral@brandeis.edu>
2022-10-05 21:59:52 -0400

    implementing healthcheck: creating Server class

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def sample():
     return "This is the {} application.".format(os.environ["APP"])
 
+@app.route('/healthcheck')
+def healthcheck():
+    return 'OK!'
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
