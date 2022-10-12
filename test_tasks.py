d39424097a5c4500c3a42d75fd38f6388a249c88
bishal-baral <bishalbaral@brandeis.edu>
2022-10-12 19:57:54 -0400

    fixed tests and updated readme

--- test_tasks.py
+++ test_tasks.py
@@ -1,6 +1,6 @@
 from tasks import healthcheck
 from utils import transform_backends_from_config
-import pytest
+
 import yaml
 
 def test_healthcheck():
@@ -8,16 +8,16 @@ def test_healthcheck():
         hosts:
           - host: www.appA.com
             servers:
-              - localhost:8888
               - localhost:8081
+              - localhost:8888
           - host: www.appB.com
             servers:
               - localhost:9081
               - localhost:4444
     ''')
     register = healthcheck(transform_backends_from_config(config))
-    assert register == []
-    assert register["www.appA.com"][0].healthy == True
-    assert register["www.appA.com"][1].healthy == False
+
     assert register["www.appB.com"][0].healthy == True
     assert register["www.appB.com"][1].healthy == False
+    assert register["www.appA.com"][0].healthy == True
+    assert register["www.appA.com"][1].healthy == False
